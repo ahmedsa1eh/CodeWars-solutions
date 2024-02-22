82164b1b0ac92dcda573175e506a700ecd6fac93
Ahmed Saleh <ahmed1a1a3a100@gmail.com>
2024-02-23 01:59:52 +0200

    another solution

--- quarteroftheyear.js
+++ quarteroftheyear.js
@@ -29,8 +29,15 @@
 // };
 
 // solution 2
+
+// const quarterOf = (month) => {
+//   return month < 3 ? 1 : month < 6 ? 2 : month < 9 ? 3 : 4;
+// };
+
+// solution 3
+
 const quarterOf = (month) => {
-  return month < 3 ? 1 : month < 6 ? 2 : month < 9 ? 3 : 4;
+  return Math.ceil(month / 3);
 };
 
-console.log(quarterOf(5));
+console.log(quarterOf(12));
