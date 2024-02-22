ef2c33df4077f4b0ced96728472ad3ea79aebfb7
Ahmed Saleh <ahmed1a1a3a100@gmail.com>
2024-02-23 01:33:56 +0200

    another solution

--- addlength.js
+++ addlength.js
@@ -1,18 +1,28 @@
-// What if we need the length of the words separated by a space to be added at the end of that same word 
+// What if we need the length of the words separated by a space to be added at the end of that same word
 // and have it returned as an array?
 /*
 Example(Input --> Output)
 "apple ban" --> ["apple 5", "ban 3"]
 "you will win" -->["you 3", "will 4", "win 3"]
 */
+
+// solution 1
+
+// function addLength(str) {
+//     let arrOfStr = str.split(' ')
+//     let output = [];
+//     let newWord = ''
+//     for (let word of arrOfStr) {
+//     newWord =  `${word} ${word.length}`
+//     output.push(newWord)
+//     }
+//     return output
+//     }
+
+// solution 2
+
 function addLength(str) {
-    let arrOfStr = str.split(' ')
-    let output = [];
-    let newWord = ''
-    for (let word of arrOfStr) {
-    newWord =  `${word} ${word.length}`
-    output.push(newWord)
-    }
-    return output
-    }
-    console.log(addLength('s'));
+  return str.split(" ").map((m) => m + " " + m.length);
+}
+
+console.log(addLength("same hereareyou stopped"));
